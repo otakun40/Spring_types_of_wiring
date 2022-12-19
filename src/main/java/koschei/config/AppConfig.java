35beeed0be086a6f499af5e98cd3ae73c48fba33
Bulat Minnigayazov <otakun40@gmail.com>
2022-12-19 15:14:54 +0300

    all done

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,31 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth) {
+        return new Needle7(deth);
+    }
+
+    @Bean
+    public static Deth8 getDeath() {
+        return new Deth8();
+    }
+}
